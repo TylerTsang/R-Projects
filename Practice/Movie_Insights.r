0028e65c51318e1da5edc7879ed8645645b419c2
Tyler Tsang <tylertsang101@gmail.com>
2019-12-30 23:19:21 -0500

    Quick update
    Adding filters in insight file

--- Practice/Movie_Insights.r
+++ Practice/Movie_Insights.r
@@ -6,5 +6,14 @@ movie <- read.csv('Movie_Data.csv')
 # setting up packages
 library(ggplot2)
 
+# Quick Summary of Data
+head(movie)
+colnames(movie)
+str(movie)
 
+# Setting filters for select genres + studios
+unique(movie$Genre)
+unique(movie$Studio)
+filt <- (movie$Genre == "action") | (movie$Genre == "adventure") | (movie$Genre == "animation") | (movie$Genre == "comedy") | (movie$Genre == "drama")
+filt2 <- (movie$Studio == 'Buena Vista Studios') | (movie$Studio == 'Universal') | (movie$Studio == 'Fox') | (movie$Studio == 'WB') | (movie$Studio == 'Sony') | (movie$Studio == 'Paramount Pictures')
 
